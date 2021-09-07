a174d3e766d4e5936488e3e07dadf804e98b76d5
thiagodorea <thiago.dorea@hotmail.com>
2021-09-07 04:18:50 -0300

    adicionando font awesome

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,8 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGhost } from '@fortawesome/free-solid-svg-icons'
+
 const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">Desenvolvido por <a href="https://github.com/thiagodorea" target="_blank" rel="noreferrer">Thiago Dorea</a></p>
+                <p className="text-light">Desenvolvido por <FontAwesomeIcon icon={faGhost} /> <a href="https://github.com/thiagodorea" target="_blank" rel="noreferrer"> Thiago Dorea</a></p>
             </div>
         </footer>
     );
