a161a4f037a40484536058c5df2e467b45ba4436
thiagodorea <thiago.dorea@hotmail.com>
2021-09-07 02:53:00 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import Navbar from "components/Navbar";
+
 function App() {
   return (
-    <div>
-     <h1 className="text-primary">Olá Mundo</h1>
-     <button className="btn btn-primary">teste</button>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Desenvolvido por <a href="https://github.com/thiagodorea" target="_blank" rel="noreferrer">Thiago Dorea</a></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
