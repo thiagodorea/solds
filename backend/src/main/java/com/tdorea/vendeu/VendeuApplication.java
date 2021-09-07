58bcfe4f496e002061e9f7a1852795e05aa83fb9
thiagodorea <thiago.dorea@hotmail.com>
2021-09-06 23:36:41 -0300

    Project created

---
+++ backend/src/main/java/com/tdorea/vendeu/VendeuApplication.java
@@ -0,0 +1,13 @@
+package com.tdorea.vendeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendeuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tdorea/vendeu/VendeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdorea.vendeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
