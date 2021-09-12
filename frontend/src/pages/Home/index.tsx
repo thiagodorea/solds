c215f4b986ea4abe39a1a4f5fe404fea35b683e9
thiagodorea <thiago.dorea@hotmail.com>
2021-09-12 03:15:18 -0300

    BarChart integration

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -10,10 +10,11 @@ const Home = () => {
         <div id="page-landing">
             <div className="content-wrapper">
                 <img className="img-logo" src={ImgSold} alt="Happy" />
+                <h1 className="txt-logo display-4"><strong>Vendeu</strong></h1>
                 <div className="container h-100">
                     <div className="row h-100 justify-content-center align-items-center">
                         <div className="jumbotron txt-body col-7">
-                            <h1 className="display-4"><strong>Vendeu</strong></h1>
+                            {/* <h1 className="display-4"><strong>Vendeu</strong></h1> */}
                             <h2><strong>Analise o desempenho das suas vendas por diferentes perspectivas</strong></h2>
                             <p className="lead mt-4">Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
                             <p><small>Atenção: O primeiro carregamento da página demora devido a hospedagem do banco de dados.</small></p>
