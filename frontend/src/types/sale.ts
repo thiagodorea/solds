c928bd9e123c4d98b86ebd6821979b36b348bdcc
thiagodorea <thiago.dorea@hotmail.com>
2021-09-12 03:46:40 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,27 @@
+import { Seller } from './seller';
+
 export type SaleSum = {
     sellerName: string,
     sum: number
+}
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    number: number;
+    size?: number;
+    numberOfElements?: number;
+    first: boolean;
+    empty?: boolean;
 }
